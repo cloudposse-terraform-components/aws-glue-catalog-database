0f02b0098b5ab9bd49df55292cf04e7b4bf4a75f
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:04 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "catalog_database_id" {
+  description = "Catalog database ID"
+  value       = module.glue_catalog_database.id
+}
+
+output "catalog_database_name" {
+  description = "Catalog database name"
+  value       = module.glue_catalog_database.name
+}
+
+output "catalog_database_arn" {
+  description = "Catalog database ARN"
+  value       = module.glue_catalog_database.arn
 }
